d97ca7c3adec66d75a25a5d1657aabfb00b58444
Grzegorz Kocjan <kkocyk@gmail.com>
2015-01-24 12:39:11 +0000

    Date generators

--- part_2_tasks.py
+++ part_2_tasks.py
@@ -1,4 +1,5 @@
 from string import ascii_lowercase
+from datetime import date, timedelta
 
 
 def generate_ascii_list():
@@ -16,8 +17,25 @@ def filter_word_list(words_list):
 def generate_squers(limit=50):
     return [x*x for x in range(1, limit+1)]
 
+
 def generate_squers_map(limit=50):
     return map(lambda x: x*x, range(1, limit+1))
+
+
+def date_generator():
+    now = date.today()
+    while True:
+        yield now
+        now += timedelta(days=1)
+
+
+def working_date_generator():
+    now = date.today()
+    while True:
+        if now.weekday() in range(0, 5):
+            yield now
+        now += timedelta(days=1)
+
 
 if __name__ == '__main__':
     print generate_ascii_list()
@@ -26,4 +44,9 @@ if __name__ == '__main__':
     
     print generate_squers()
     print generate_squers_map()
-    
+
+    d = working_date_generator()
+    for x in range(10):
+        day = d.next()
+        print '{}: {}'.format(day, day.weekday())
+
