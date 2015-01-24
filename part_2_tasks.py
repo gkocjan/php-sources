893585eab600a4a4c026bbb7e8d64eec7765d8fc
Grzegorz Kocjan <kkocyk@gmail.com>
2015-01-24 11:32:24 +0000

    Add squere tasks

--- part_2_tasks.py
+++ part_2_tasks.py
@@ -13,7 +13,17 @@ def filter_word_list(words_list):
     return [x for x in words_list if len(x) < 5]
 
 
+def generate_squers(limit=50):
+    return [x*x for x in range(1, limit+1)]
+
+def generate_squers_map(limit=50):
+    return map(lambda x: x*x, range(1, limit+1))
+
 if __name__ == '__main__':
     print generate_ascii_list()
     print generate_ascii_codes_map()
     print filter_word_list(['ala', 'maaaaaaaaa', 'kota'])
+    
+    print generate_squers()
+    print generate_squers_map()
+    
